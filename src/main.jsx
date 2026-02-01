9757141b51c650cc78a66559e1ff9507aa1f6bfe
razeem <appurazeem2000@email.com>
2026-02-01 18:00:36 +0530

    feat: setup vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-const redirect = sessionStorage.getItem("redirect");
-if (redirect) {
-  sessionStorage.removeItem("redirect");
-  window.history.replaceState(null, "", redirect);
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
